6de3fae59722403cb43b3b1709f71c88482a3aab
Orane31 <orane.chaper@gmail.com>
2020-08-03 23:34:23 +0200

    first commit

---
+++ app/models/gossip.rb
@@ -0,0 +1,6 @@
+class Gossip < ApplicationRecord
+    belongs_to :user
+    has_many :tagossips
+    has_many :tags, through: :tagossips
+
+end
---
+++ app/models/tagossip.rb
@@ -0,0 +1,4 @@
+class Tagossip < ApplicationRecord
+    belongs_to :gossip
+    belongs_to :tag
+end
